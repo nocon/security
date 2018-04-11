1fb696ab78e88666b24ef5abb6e978eb7b95c1f7
nocon <nocon@users.noreply.github.com>
2018-04-11 09:06:49 +0100

    Go is not necessary

--- install.sh
+++ install.sh
@@ -11,11 +11,3 @@ curl -o cfssljson https://pkg.cfssl.org/R1.2/cfssljson_$system
 chmod +x cfssl cfssljson
 sudo mv cfssl /usr/local/bin/cfssl
 sudo mv cfssljson /usr/local/bin/cfssljson
-
-# install go
-sudo curl -o go.tar.gz https://storage.googleapis.com/golang/go1.10.1.$system.tar.gz
-sudo tar -xvf go.tar.gz
-sudo mv go /usr/local
-
-# TODO: Add to profile
-export PATH=$PATH:/usr/local/go/binc
